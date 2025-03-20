3b141103e832d122cd25f68b39e567937d27936e
Adam Parker <foreverska@protonmail.com>
2025-03-19 21:00:35 -0500

    Version bump and changelog additions

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description="Buffalo Gym environment",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="0.3.0",
+    version="0.3.1",
     author="foreverska",
     install_requires=["gymnasium>=0.26.0", "numpy"],
     keywords="gymnasium, gym",
